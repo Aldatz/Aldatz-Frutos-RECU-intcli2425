143bd36547d91f194a11258ac1ab9805b0b0f995
Aldatz <aldatz.frutos@ikasle.aeg.eus>
2025-01-29 12:40:54 +0100

    fix

--- src/helpers/countBillagerByProfesion.ts
+++ src/helpers/countBillagerByProfesion.ts
@@ -1,11 +1,9 @@
 import { epicFortress } from "../types/megaEpicFortress";
 
 export function countBillagerByProfesion(fortress: epicFortress) {
-    let workers;
     let response = []
     for (let i = 0; i < fortress.inhabitants.roles[0].professions.length; i++) {
-        workers = fortress.inhabitants.roles[0].professions[i]
-        let towerInfo = [workers = fortress.inhabitants.roles[0].professions[i].count, workers = fortress.inhabitants.roles[0].professions[i].type]
+        let towerInfo = [fortress.inhabitants.roles[0].professions[i].count, fortress.inhabitants.roles[0].professions[i].type]
         response[i] = towerInfo;
 
     }
